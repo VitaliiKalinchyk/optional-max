ea0163998838cd54f7c6dd36a8c27c7d917953ad
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-22 15:18:20 +0300

    MaxMethod with OptionalInt

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -4,6 +4,15 @@ import java.util.OptionalInt;
 
 public class MaxMethod {
     public static OptionalInt max(int[] values) {
-        throw new UnsupportedOperationException();
+
+        if (values != null && values.length > 0) {
+            int max=values[0];
+            for (int i:values ) {
+                max = Math.max(max,i);
+            }
+            return OptionalInt.of(max);
+        }
+
+        return OptionalInt.empty();
     }
 }
